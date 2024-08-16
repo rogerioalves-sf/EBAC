3d01b48f67178faa8267dd80ed062517db06ddd5
Rogério <146471734+rogerioalves-sf@users.noreply.github.com>
2024-08-16 18:41:45 -0300

    exercicio

---
+++ exercicio_ts.js
@@ -0,0 +1,7 @@
+"use strict";
+function somar(A, B) {
+    return A * B;
+}
+function saudacao(nome) {
+    return `Olá, ${nome}`;
+}
---
+++ exercicio_ts.ts
@@ -0,0 +1,7 @@
+function somar(A: number, B: number): number {
+    return A * B;
+}
+
+function saudacao(nome: string): string {
+    return `Olá, ${nome}`;
+}
